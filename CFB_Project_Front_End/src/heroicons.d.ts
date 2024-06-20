0424cf7ed9e85cf2604a818976530fc5e40db8f9
Shijitha-S <Shijitha-S@DESKTOP-7NRD6B2>
2024-06-20 13:50:30 -0400

    Home Page and Navigation page chnages

---
+++ CFB_Project_Front_End/src/heroicons.d.ts
@@ -0,0 +1,7 @@
+declare module '@heroicons/react/outline' {
+    import * as React from 'react';
+  
+    export const LoginIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    // Add other icons as needed
+  }
+  
