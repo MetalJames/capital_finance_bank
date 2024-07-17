7eed0bbf1e74430f0df126deda000d7ce0bb5408
SHIKHASHARMA21 <156105707+SHIKHASHARMA21@users.noreply.github.com>
2024-07-17 11:39:59 -0400

    Update SignUp.tsx
    Signup page updated

--- CFB_Project_Front_End/src/pages/SignUp.tsx
+++ CFB_Project_Front_End/src/pages/SignUp.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-//import general reusable input componnent
 import GeneralInput from "../components/GeneralInput";
 
 const SignUp = () => {
 
-    //setting variables here
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -21,7 +19,6 @@ const SignUp = () => {
     const [errors, setErrors] = useState<{ [key: string]: string }>({});
     const [message, setMessage] = useState('');
 
-    //create array of provinces here to make it easy to read
     const provinces = [
         { label: 'Select Province', value: '' },
         { label: 'Ontario', value: 'ON' },
@@ -36,10 +33,8 @@ const SignUp = () => {
         { label: 'Newfoundland and Labrador', value: 'NL' },
     ];
 
-    //email validation using regex - so correct email will be entered
     const regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    //submittion functions with all validations
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         let valid = true;
@@ -97,7 +92,6 @@ const SignUp = () => {
             return;
         }
 
-        //resetting all the fields
         alert('Thank you for signing up! This is just a demo :)');
         setFirstName('');
         setLastName('');
@@ -115,26 +109,23 @@ const SignUp = () => {
         setMessage('');
     }
 
-    // Helper function to get error message for a field
     const getErrorMessage = (field: string) => {
         return errors[field] && <p className="text-red-500 text-xs">{errors[field]}</p>;
     }
 
-    //html starts here
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className="text-center">
-                <h1 className="text-3xl text-bold mb-4">Create Your Account</h1>
-                <p>
-                    or <Link to="/login" className="text-sky-400 cursor-pointer">Log In</Link>
+        <div className="flex flex-col justify-center items-center h-screen bg-[#EADBC8]">
+            <div className="text-center mb-6">
+                <h1 className="text-3xl font-bold mb-2">EasyWeb Register</h1>
+                <p className="text-sm">
+                    Already have an account? <Link to="/login" className="text-[#DEAC80] cursor-pointer">Please Login</Link>
                 </p>
             </div>
-            {/* form submittion starts here */}
-            <form onSubmit={handleSubmit} className="w-full max-w-md">
+            <form onSubmit={handleSubmit} className="w-full max-w-lg bg-[#F7E9D7] p-8 rounded-lg shadow-md">
                 <div className="grid grid-cols-2 gap-4">
                     <div className="flex flex-col">
                         <label className="text-sm font-semibold" htmlFor="firstName">First Name</label>
-                        <GeneralInput id="firstNmae" text="text" placeholder="First Name" value={firstName} propFunction={(e) => setFirstName(e.target.value)} error={!!errors.firstName} />
+                        <GeneralInput id="firstName" text="text" placeholder="First Name" value={firstName} propFunction={(e) => setFirstName(e.target.value)} error={!!errors.firstName} />
                         {getErrorMessage('firstName')}
                     </div>
                     <div className="flex flex-col">
@@ -142,30 +133,25 @@ const SignUp = () => {
                         <GeneralInput id="lastName" text="text" placeholder="Last Name" value={lastName} propFunction={(e) => setLastName(e.target.value)} error={!!errors.lastName} />
                         {getErrorMessage('lastName')}
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col col-span-2">
                         <label className="text-sm font-semibold" htmlFor="email">Email Address</label>
                         <GeneralInput text="text" placeholder="Email Address" value={email} propFunction={(e) => setEmail(e.target.value)} error={!!errors.email} />
                         {getErrorMessage('email')}
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col col-span-2">
                         <label className="text-sm font-semibold" htmlFor="phone">Phone Number</label>
                         <GeneralInput text="text" placeholder="Phone Number" value={phone} propFunction={(e) => setPhone(e.target.value)} error={!!errors.phone} />
                         {getErrorMessage('phone')}
                     </div>
-                    <div className="flex flex-col">
-                        <label className="text-sm font-semibold" htmlFor="unitNumber">Unit Number (optional)</label>
-                        <GeneralInput text="text" placeholder="Unit Number" value={unitNumber} propFunction={(e) => setUnitNumber(e.target.value)} error={!!errors.unitNumber} />
-                        {getErrorMessage('unitNumber')}
-                    </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col col-span-2">
                         <label className="text-sm font-semibold" htmlFor="streetAddress">Street Address</label>
                         <GeneralInput text="text" placeholder="Street Address" value={streetAddress} propFunction={(e) => setStreetAddress(e.target.value)} error={!!errors.streetAddress} />
                         {getErrorMessage('streetAddress')}
                     </div>
-                    <div className="flex flex-col">
-                        <label className="text-sm font-semibold" htmlFor="postalCode">Postal Code</label>
-                        <GeneralInput text="text" placeholder="Postal Code" value={postalCode} propFunction={(e) => setPostalCode(e.target.value)} error={!!errors.postalCode} />
-                        {getErrorMessage('postalCode')}
+                    <div className="flex flex-col col-span-2">
+                        <label className="text-sm font-semibold" htmlFor="unitNumber">Unit Number (optional)</label>
+                        <GeneralInput text="text" placeholder="Unit Number" value={unitNumber} propFunction={(e) => setUnitNumber(e.target.value)} error={!!errors.unitNumber} />
+                        {getErrorMessage('unitNumber')}
                     </div>
                     <div className="flex flex-col">
                         <label className="text-sm font-semibold" htmlFor="city">City</label>
@@ -176,7 +162,7 @@ const SignUp = () => {
                         <label className="text-sm font-semibold" htmlFor="province">Province</label>
                         <select
                             id="province"
-                            className={`text-[#000] my-2 w-full border-4 ${errors.province ? 'border-[red]' : 'border-[#243c5a]'}`}
+                            className={`my-2 w-full border-2 ${errors.province ? 'border-red-500' : 'border-gray-300'} rounded-lg p-2`}
                             value={province}
                             onChange={(e) => setProvince(e.target.value)}
                         >
@@ -187,32 +173,38 @@ const SignUp = () => {
                         {getErrorMessage('province')}
                     </div>
                     <div className="flex flex-col">
+                        <label className="text-sm font-semibold" htmlFor="postalCode">Postal Code</label>
+                        <GeneralInput text="text" placeholder="Postal Code" value={postalCode} propFunction={(e) => setPostalCode(e.target.value)} error={!!errors.postalCode} />
+                        {getErrorMessage('postalCode')}
+                    </div>
+                    <div className="flex flex-col col-span-2">
                         <label className="text-sm font-semibold" htmlFor="password">Password</label>
                         <GeneralInput text="password" placeholder="Password" value={password} propFunction={(e) => setPassword(e.target.value)} error={!!errors.password} />
                         {getErrorMessage('password')}
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col col-span-2">
                         <label className="text-sm font-semibold" htmlFor="confirmPassword">Confirm Password</label>
                         <GeneralInput text="password" placeholder="Confirm Password" value={confirmPassword} propFunction={(e) => setConfirmPassword(e.target.value)} error={!!errors.confirmPassword} />
                         {getErrorMessage('confirmPassword')}
                     </div>
                 </div>
-                {message && (
-                    <div className="my-1 text-[red] font-light text-[12px]">
-                        {message}
-                    </div>
-                )}
-                <div className="flex justify-between items-center my-2">
-                    <div>
-                        <input type="checkbox" checked={agree} className="mr-2" onChange={() => setAgree(!agree)} />
-                        <label htmlFor="agreement" className="text-sm">Agree To Policies and Conditions</label>
-                        {getErrorMessage('agreement')}
-                    </div>
+                <div className="flex items-center mt-4">
+                    <input
+                        type="checkbox"
+                        id="agreement"
+                        className="mr-2"
+                        checked={agree}
+                        onChange={(e) => setAgree(e.target.checked)}
+                    />
+                    <label htmlFor="agreement" className="text-sm">I agree to the <Link to="/terms" className="text-[#DEAC80] cursor-pointer">terms and conditions</Link></label>
+                    {errors["agreement"] && (
+                        <p className="text-red-500 text-xs">{errors["agreement"]}</p>
+                    )}
                 </div>
-                <button type="submit" className='mt-4 my-2 bg-blue-700 text-white w-full p-2 hover:bg-blue-900 transition-colors duration-200'>Sign Up</button>
+                <button type="submit" className="w-full bg-[#DEAC80] text-white py-2 rounded-lg mt-4 hover:bg-[#c79463]">Sign Up</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default SignUp;
